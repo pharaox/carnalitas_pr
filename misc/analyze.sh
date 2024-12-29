b6275f1c8c0ca2d80bd99aafd9651f219d544eb2
pharaox <pharaox@tutanota.com>
2024-12-30 00:25:27 +0200

    Integrate with other vanilla events: * Make characters from chariot_race.4030, tour_travel.3007, tour_travel.3140 proper prostitutes * Track character creation and removal * Reduce frequency of beauty traits * Override carn_can_be_prostitute_trigger, carn_prostitution_random_events_pulse, and carn_prostitution_random_events.0010 to fix issues

--- misc/analyze.sh
+++ misc/analyze.sh
@@ -14,16 +14,19 @@ echo
 
 # Health
 echo "Prostitute got STD: $(grep -a "Prostitute got STD" $LOGFILE | wc -l)"
-echo "- lovers_pox: $(grep -a "disease_type: lovers_pox" $LOGFILE | wc -l)"
-echo "- great_pox: $(grep -a "disease_type: great_pox" $LOGFILE | wc -l)"
-echo "- early_great_pox: $(grep -a "disease_type: early_great_pox" $LOGFILE | wc -l)"
+echo "- lovers_pox: $(grep -a "Prostitute got STD, disease_type: lovers_pox" $LOGFILE | wc -l)"
+echo "- great_pox: $(grep -a "Prostitute got STD, disease_type: great_pox" $LOGFILE | wc -l)"
+echo "- early_great_pox: $(grep -a "Prostitute got STD, disease_type: early_great_pox" $LOGFILE | wc -l)"
 echo
 
-# Events
+# Stress coping events
 echo "Stress Coping: Wanton Ways event fired: $(grep -a "Stress Coping: Wanton Ways event fired" $LOGFILE | wc -l)"
 echo "Stress Coping: The Price of Passion event fired: $(grep -a "Stress Coping: The Price of Passion event fired" $LOGFILE | wc -l)"
-echo "Stress Coping: Wanton Ways vanilla event fired: $(grep -a "Stress Coping: Wanton Ways vanilla event fired" $LOGFILE | wc -l)"
-echo "Stress Coping: The Price of Passion vanilla event fired: $(grep -a "Stress Coping: The Price of Passion vanilla event fired" $LOGFILE | wc -l)"
+echo
+
+# Carnalitas events
+echo "cpr_prostitution_random_events.0010 event fired: $(grep -a "cpr_prostitution_random_events.0010 event fired" $LOGFILE | wc -l)"
+echo "- client: $(grep -a "cpr_prostitution_random_events.0010 event fired, client: [^ ]" $LOGFILE | wc -l)"
 echo
 
 # Effects
@@ -33,6 +36,12 @@ echo "Using vanilla_rakish_brothel_night_effect: $(grep -a "Using vanilla_rakish
 echo "- from_stress_loss_decision: $(grep -a "Using vanilla_rakish_brothel_night_effect, from_stress_loss_decision: yes" $LOGFILE | wc -l)"
 echo
 
-# Character creation
+# Character creation and removal
 echo "Creating prostitute: $(grep -a "Creating prostitute" $LOGFILE | wc -l)"
+echo "- generic: $(grep -a "Creating prostitute, origin: generic" $LOGFILE | wc -l)"
+echo "- chariot_race_4030: $(grep -a "Creating prostitute, origin: chariot_race_4030" $LOGFILE | wc -l)"
+echo "- tour_travel_3007: $(grep -a "Creating prostitute, origin: tour_travel_3007" $LOGFILE | wc -l)"
+echo "- tour_travel_3140: $(grep -a "Creating prostitute, origin: tour_travel_3140" $LOGFILE | wc -l)"
+echo "Removing prostitute: $(grep -a "Removing prostitute" $LOGFILE | wc -l)"
+echo "- chariot_race_4030: $(grep -a "Removing prostitute, origin: chariot_race_4030" $LOGFILE | wc -l)"
 echo
